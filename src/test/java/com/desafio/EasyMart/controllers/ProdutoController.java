9d7246d0b538ddf7306679c643701d4f5e62fbe0
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-31 15:00:14 -0300

    feat: adicionando o metodo buscarPorId na classe ProdutoController

--- src/test/java/com/desafio/EasyMart/controllers/ProdutoController.java
+++ src/test/java/com/desafio/EasyMart/controllers/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.desafio.EasyMart.controllers;
 
 import com.desafio.EasyMart.dtos.ProdutoDTO;
+import com.desafio.EasyMart.models.ProdutoModel;
 import com.desafio.EasyMart.services.ProdutoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -31,9 +33,25 @@ public class ProdutoController {
                         produto.getEstoque(),
                         produto.getCategoria().toString() // Convertendo CategoriaProduto para String
                 )).toList();
-        return ResponseEntity.ok(produtos)
+        return ResponseEntity.ok(produtos);
 
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> buscarPorId(@PathVariable Long id) {
+        Optional<ProdutoModel> produtoModel = produtoService.buscarPorId(id);
+        if (produtoModel.isPresent()) {
+            ProdutoDTO produtoDTO = new ProdutoDTO(
+                    produtoModel.get().getId(),
+                    produtoModel.get().getNome(),
+                    produtoModel.get().getDescricao(),
+                    null,
+                    produtoModel.get().getEstoque(),
+                    produtoModel.get().getCategoria().toString()
+            );
+            return ResponseEntity.ok(produtoDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
