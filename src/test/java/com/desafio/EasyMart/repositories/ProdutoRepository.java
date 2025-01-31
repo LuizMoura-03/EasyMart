f62f1d4d0899bf0517709a49efd1bb2b5fa2b9a7
LuizFernando <barbosa.luiz@zup.com.br>
2025-01-31 14:02:53 -0300

    feat: adicionado o método existsByNome na ProdutoRepository

--- src/test/java/com/desafio/EasyMart/repositories/ProdutoRepository.java
+++ src/test/java/com/desafio/EasyMart/repositories/ProdutoRepository.java
@@ -4,4 +4,5 @@ import com.desafio.EasyMart.models.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+    boolean existsByNome(String nome);
 }
