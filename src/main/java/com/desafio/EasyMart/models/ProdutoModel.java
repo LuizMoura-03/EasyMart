fecc7797736eaad7e9660561063c9f564c50e4e5
LuizFernando <barbosa.luiz@zup.com.br>
2025-02-05 10:03:11 -0300

    refactor: melhorado a estrutura da classe ProdutoModel com validaçoes  adicionados metodos auxiliares e mapeamento JPA

--- src/main/java/com/desafio/EasyMart/models/ProdutoModel.java
+++ src/main/java/com/desafio/EasyMart/models/ProdutoModel.java
@@ -12,23 +12,27 @@ public class ProdutoModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "O nome é obrigatorio")
+    @NotBlank(message = "O campo 'nome' é obrigatorio")
     @Size(min = 2, max = 100, message = "O nome deve ter enre 2 e 100 caracteres.")
+    @Column(name = "nome", nullable = false, length = 100)
     private String nome;
 
-    @Size(max = 500, message = "A descrição não pode ultrapassar os 500 carateres.")
+    @Size(max = 500, message = "O campo 'descrição' não pode ultrapassar os 500 carateres.")
+    @Column(name = "descricao", length = 500)
     private String descricao;
 
-    @NotNull(message = "O preço é obrigatório.")
-    @DecimalMin(value = "0.01", message = "O preço deve ser maior que 0.")
+    @NotNull(message = "O campo 'preço' é obrigatório.")
+    @DecimalMin(value = "0.01", message = "O campo 'preço' deve ser maior que 0.")
+    @Column(name = "preco", nullable = false)
     private Double preco;
 
-    @NotNull(message = "A quantidade em estoque é obrigatoria.")
-    @Min(value = 0, message = "A quantidade em estoque deve ser maior ou igaul a 0.")
+    @NotNull(message = "O campo estoque' é obrigatoria.")
+    @Min(value = 0, message = "O campo 'estoque' deve ser maior ou igaul a 0.")
     private Integer estoque;
 
-    @NotNull(message = "A categoria é obrigatoria")
+    @NotNull(message = "O campo 'categoria' é obrigatoria")
     @Enumerated(EnumType.STRING)
+    @Column(name = "categoria", nullable = false)
     private CategoriaProduto categoria;
 
     public ProdutoModel() {}
@@ -90,4 +94,30 @@ public class ProdutoModel {
     public void setCategoria(CategoriaProduto categoria) {
         this.categoria = categoria;
     }
+
+    @Override
+    public String toString() {
+        return "ProdutoModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                ", estoque=" + estoque +
+                ", categoria=" + categoria +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProdutoModel that = (ProdutoModel) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
 }
